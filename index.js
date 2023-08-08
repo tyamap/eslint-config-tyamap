8dbfec70b4eaac3cdadc78764ab63ef94ccec4d7
tyamap <tom333my@outlook.com>
2023-08-08 22:29:20 +0900

    add semicolon lint

--- index.js
+++ index.js
@@ -1,5 +1,19 @@
 module.exports = {
+  parserOptions: {},
+  env: {
+    browser: true,
+  },
+  extends: ["eslint:recommended"],
+  plugins: [],
+  globals: {},
   rules: {
-    semi: [2, "always"],
+    "no-console": "warn",
+    // no-semicolon
+    semi: ["error", "never", { beforeStatementContinuationChars: "never" }],
+    "semi-spacing": ["error", { after: true, before: false }],
+    "semi-style": ["error", "first"],
+    "no-extra-semi": "error",
+    "no-unexpected-multiline": "error",
+    "no-unreachable": "error",
   },
 };
